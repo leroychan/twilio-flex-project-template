c274b1f1aefe3922b8e0185e95e795ad5a6b0b61
Leroy Chan <5236195+leroychan@users.noreply.github.com>
2023-06-16 21:21:05 +0800

    fix: chat to video

--- plugin-flex-ts-template-v2/src/feature-library/chat-to-video-escalation/custom-components/SwitchToVideo/SwitchToVideo.tsx
+++ plugin-flex-ts-template-v2/src/feature-library/chat-to-video-escalation/custom-components/SwitchToVideo/SwitchToVideo.tsx
@@ -75,12 +75,8 @@ const SwitchToVideo: React.FunctionComponent<SwitchToVideoProps> = ({ task, conv
           Notifications.showNotification(ChatToVideoNotification.FailedVideoLinkNotification);
           return;
         }
-        console.log(`-------`);
-        console.log('Leroy 1', templates[StringTemplates.InviteMessage]({ videoLink: response.full_url }));
-        console.log('Leroy 2', response.full_url);
-        console.log(`-------`)
         Actions.invokeAction('SendMessage', {
-          body: templates[StringTemplates.InviteMessage]({ videoLink: response.full_url }),
+          body: `${templates[StringTemplates.InviteMessage]}: ${response.full_url}`,
           conversation,
           messageAttributes: {
             hasVideo: true,
